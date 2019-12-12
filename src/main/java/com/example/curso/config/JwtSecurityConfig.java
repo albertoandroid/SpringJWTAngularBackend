89b05be6f5f9acb4b33b548c7e310cc4ddc4a67e
Alberto <info@androiddesdecero.com>
2019-12-12 15:19:31 +0100

    2

--- src/main/java/com/example/curso/config/JwtSecurityConfig.java
+++ src/main/java/com/example/curso/config/JwtSecurityConfig.java
@@ -6,8 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.stereotype.Component;
 
 import com.example.curso.security.JwtAuthenticationEntryPoint;
@@ -37,5 +39,19 @@ public class JwtSecurityConfig extends WebSecurityConfigurerAdapter {
 		filter.setAuthenticationSuccessHandler(new JwtSuccessHandler());
 		return filter;
 	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception{
+		http.cors()
+			.and()
+			.csrf()
+			.disable()
+			.exceptionHandling().authenticationEntryPoint(entryPoint)
+			.and()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+	
+	
+	
 	
 }
