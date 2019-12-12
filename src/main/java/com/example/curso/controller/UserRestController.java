94111824df377f3c8c9709127f4dfeac9086fa32
Alberto <info@androiddesdecero.com>
2019-12-12 15:27:59 +0100

    4

--- src/main/java/com/example/curso/controller/UserRestController.java
+++ src/main/java/com/example/curso/controller/UserRestController.java
@@ -1,5 +1,7 @@
 package com.example.curso.controller;
 
+import java.util.Collections;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.curso.entity.User;
+import com.example.curso.model.JwtUser;
+import com.example.curso.security.JwtGenerator;
 import com.example.curso.service.IUserService;
 
 @RestController
@@ -18,11 +22,18 @@ public class UserRestController {
 	@Autowired
 	private IUserService userService;
 	
+	@Autowired
+	private JwtGenerator jwtGenerator;
+	
 	@PostMapping(value= "/user")
 	public ResponseEntity<?> addUser(@RequestBody User user){
 		if(userService.findUser(user)==null) {
 			userService.save(user);
-			return new ResponseEntity<Void>(HttpStatus.CREATED);
+			User userDb = userService.checkUserLogin(user);
+			JwtUser jwtUser = new JwtUser();
+			jwtUser.setId(userDb.getId());
+			jwtUser.setUserName(userDb.getEmail());
+			return new ResponseEntity<>((Collections.singletonMap("jwtToken", jwtGenerator.generate(jwtUser))),HttpStatus.CREATED);
 		}else {
 			return new ResponseEntity<Void>(HttpStatus.CONFLICT);
 		}
