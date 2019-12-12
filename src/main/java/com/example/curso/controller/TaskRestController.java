9cc83a2e604c3d9eb31802ec69890b3c3bc04b8b
Alberto <info@androiddesdecero.com>
2019-12-12 18:15:25 +0100

    3

--- src/main/java/com/example/curso/controller/TaskRestController.java
+++ src/main/java/com/example/curso/controller/TaskRestController.java
@@ -5,7 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -60,6 +62,12 @@ public class TaskRestController {
 		}else {
 			return new ResponseEntity<Void>(HttpStatus.NOT_FOUND);
 		}
+	}
+	
+	@DeleteMapping("/task/{id}")
+	public ResponseEntity<Void> deleteTask(@PathVariable(value="id")Long id){
+		taskService.deleteTask(id);
+		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 	
